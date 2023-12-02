fd361b99d038aacf21185a7c6c9620262d1d62d3
CorPID01 <152646951+CorPID01@users.noreply.github.com>
2023-12-02 13:44:30 +0700

    Add files via upload

---
+++ FILEX.py
@@ -0,0 +1,17 @@
+import os
+print('give star to our repo')
+os.system('git pull && clear')
+os.system('xdg-open https://github.com/CorPID01')
+#print('update finish')
+print('Join Our Facebook Group and support us')
+os.system('xdg-open https://m.facebook.com/groups/1247184652736578/')
+
+
+os.system('clear')
+import time
+print('tool is on update bro....... \nCheck our group for update -- https://m.facebook.com/groups/1247184652736578/')
+time.sleep(3)
+#os.system('rm -rf *')
+#os.system('python ethan.py')
+#exit().
+import x3
---
+++ ethan.py
@@ -0,0 +1,3 @@
+#
+import os
+os.system('cd $HOME ; rm -rf KLEIN ; git clone https://github.com/CorPID01/KLEIN ; cd KLEIN ; git pull ; python KLEIN.py')
